11970e54f5f20424970097c696a24384e800018e
4thidiot <tendingtowardsinfinity@gmail.com>
2025-04-19 22:32:58 +0530

    Create A - Young Physicist.cpp

---
+++ A - Young Physicist.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	int sum_a=0, sum_b=0, sum_c=0;
+	while(t--)
+	{
+	    int a, b, c;
+	    cin>>a>>b>>c;
+	    
+	    sum_a+=a;
+	    sum_b+=b;
+	    sum_c+=c;
+	}
+    if(sum_a==0 && sum_b==0 && sum_c==0)
+        cout<<"YES";
+    else 
+        cout<<"NO";
+        
+    return 0;    
+}
