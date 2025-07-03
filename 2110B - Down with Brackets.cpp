0183270a537f00a9aeba7462a6660d38e3d30f2e
4thidiot <tendingtowardsinfinity@gmail.com>
2025-07-03 23:53:57 +0530

    Update 2110B - Down with Brackets.cpp

--- 2110B - Down with Brackets.cpp
+++ 2110B - Down with Brackets.cpp
@@ -1,29 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using ld = long double;
-#define INF(t) numeric_limits<t>::max()
-
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0);
-
-	int tc; cin >> tc;
-	while(tc--) {
-		string s; cin >> s;
-		int n = s.size();
-		map<int, int> depth_enter;
-		int v = 0;
-		bool ans = false;
-		for(int i = 0; i < n; i++) {
-			if(s[i] == '(') v++;
-			else v--;
-
-			if(i > 0 && i < n - 1 && v == 0) ans = true;
-		}
-		if(ans) cout << "YES\n";
-		else cout << "NO\n";
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    int n = s.size();
+	    int a = 0; //balance factor
+	    bool ans = false;
+	    for(int i=0; i<n; i++)
+	    {
+	        if(s[i] == '(')
+	            a++;
+	        else
+	            a--;
+	            
+	        if(a==0 && i!=n-1)
+	        {
+	            ans = true;
+	            break;
+	        }     
+	    }
+	    if(ans)
+	        cout<<"YES"<<'\n';
+	    else
+	        cout<<"NO"<<'\n';
 	}
-
-	return 0;
+    return 0;
 }
+
