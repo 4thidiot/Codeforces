ca42eedbe36243c9f979d1729f439a37ab030bca
4thidiot <tendingtowardsinfinity@gmail.com>
2025-08-05 23:13:27 +0530

    Create 459A - Pashmak and Garden.cpp

---
+++ 1200/459A - Pashmak and Garden.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+	ll x1, y1, x2, y2;
+	cin>>x1>>y1>>x2>>y2;
+	
+	if ( (x1 != x2 && y1 != y2) && abs(x1 - x2) != abs(y1 - y2) )
+	    cout << -1;
+	else if (x1 == x2)
+        cout << x1 + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2 << '\n';
+    else if (y1 == y2)
+        cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << '\n';
+    else
+        cout << x1 << " " << y2 << " " << x2 << " " << y1 << '\n';
+        
+    return 0;
+}
