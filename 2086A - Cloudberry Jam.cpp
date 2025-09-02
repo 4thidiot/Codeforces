0628e89c9896eec8ef7419604ef031c488fefba2
4thidiot <tendingtowardsinfinity@gmail.com>
2025-09-02 23:16:14 +0530

    Create 2086A - Cloudberry Jam.cpp

---
+++ 2086A - Cloudberry Jam.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n;
+	    cin>>n;
+	    cout<<2*n<<'\n';
+	}
+    return 0;
+}
