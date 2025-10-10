6c7a1a1243c7bf347cfe29dafb63677856958233
4thidiot <tendingtowardsinfinity@gmail.com>
2025-10-10 23:10:14 +0530

    Create 1926A - Vlad and the Best of Five.cpp

---
+++ 1926A - Vlad and the Best of Five.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    string s;
+	    cin>>s;
+	    
+	    ll a=0, b=0;
+	    
+	    for(auto it : s)
+	    {
+	        if(it=='A')
+	            a++;
+	        else
+	            b++;
+	    }
+	    
+	    if(a>b)
+	        cout<<"A\n";
+	    else
+	        cout<<"B\n";
+	}
+    return 0;
+}
