796491f9afc170eab1c7730ec36792d11958c648
4thidiot <tendingtowardsinfinity@gmail.com>
2025-10-16 23:12:44 +0530

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+	ll n, m, k;
+	cin>>n>>m>>k;
+	
+	if(n<=m && n<=k)
+	    cout<<"YES\n";
+	else
+	    cout<<"NO\n";
+
+    return 0;
+}
