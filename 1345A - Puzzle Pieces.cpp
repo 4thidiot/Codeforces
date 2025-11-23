1c50729b7b8ac63d5f471edd5071e9a771f09533
4thidiot <tendingtowardsinfinity@gmail.com>
2025-11-23 23:21:48 +0530

    Create 1345A - Puzzle Pieces.cpp

---
+++ 1345A - Puzzle Pieces.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t; 
+    cin >> t;
+    while(t--) 
+    {
+        long long n, m;
+        cin >> n >> m;
+        
+        if (n == 1 || m == 1 || (n == 2 && m == 2))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+  
