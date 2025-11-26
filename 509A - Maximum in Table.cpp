a50262491398824d5d4cf1afc0cbcfae9cbcab88
4thidiot <tendingtowardsinfinity@gmail.com>
2025-11-26 22:43:28 +0530

    Create 509A - Maximum in Table.cpp

---
+++ 509A - Maximum in Table.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    int a[11][11];
+
+    for (int i = 1; i <= n; i++)
+    {
+        a[i][1] = 1;
+        a[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 2; j <= n; j++)
+            a[i][j] = a[i - 1][j] + a[i][j - 1];
+    }
+
+    cout << a[n][n] << '\n';
+
+    return 0;
+}
