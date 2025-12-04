97790df9199a5bfff1b3846636b7d88dfefe6912
4thidiot <tendingtowardsinfinity@gmail.com>
2025-12-04 23:28:58 +0530

    Create 2048A - Kevin and Combination Lock.cpp

---
+++ 2048A - Kevin and Combination Lock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin >> x;
+        if (x % 33 == 0) 
+            cout << "YES\n";
+        else 
+            cout << "NO\n";
+    }
+    
+    return 0;
+}
