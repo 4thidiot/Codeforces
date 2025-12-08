bf628fcf6c804dc1d044f066a7df4183d0ede323
4thidiot <tendingtowardsinfinity@gmail.com>
2025-12-08 23:28:56 +0530

    Create 1787A - Exponential Equation.cpp

---
+++ 1787A - Exponential Equation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        long long n;
+        cin >> n;
+
+        if(n % 2 == 1)
+        {
+            cout << -1 << '\n';
+        }
+        else
+        {
+            cout << 1 << " " << n / 2 << '\n';
+        }
+    }
+
+    return 0;
+}
