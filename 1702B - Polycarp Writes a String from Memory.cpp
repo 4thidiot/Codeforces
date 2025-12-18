a6a976d7959e852da414e3f6190f741e493a14ec
4thidiot <tendingtowardsinfinity@gmail.com>
2025-12-18 23:36:19 +0530

    Create 1702B - Polycarp Writes a String from Memory.cpp

---
+++ 1702B - Polycarp Writes a String from Memory.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    set<char> v;
+    int ans = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        v.insert(s[i]);
+        if (v.size() > 3) {
+            ans++;
+            v.clear();
+            v.insert(s[i]);
+        }
+    }
+
+    if (!v.empty()) ans++;
+    cout << ans << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
