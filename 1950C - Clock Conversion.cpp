2a8c70627148c40b4a59dc892bb38acfa446a0c2
4thidiot <tendingtowardsinfinity@gmail.com>
2026-01-07 22:56:18 +0530

    Create 1950C - Clock Conversion.cpp

---
+++ 1950C - Clock Conversion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() 
+{
+    int h, m;
+    char c;
+    cin >> h >> c >> m;
+
+    string am = (h < 12 ? " AM" : " PM");
+    h = (h % 12 ? h % 12 : 12);
+
+    cout << (h < 10 ? "0" : "") << h
+         << c
+         << (m < 10 ? "0" : "") << m
+         << am << '\n';
+}
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
