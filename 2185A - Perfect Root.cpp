7b420fad2f80b2c26b6866f5c018ba3464c8541a
4thidiot <tendingtowardsinfinity@gmail.com>
2026-01-30 23:33:26 +0530

    Create 2185A - Perfect Root.cpp

---
+++ 2185A - Perfect Root.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    ll n;
+	cin>>n;
+	for(int i=1; i<=n; i++)
+	    cout<<i<<" ";
+	cout<<'\n';
+}
+
+int main() 
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	    solve();
+    
+    return 0;
+}
