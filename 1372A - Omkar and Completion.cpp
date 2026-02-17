578a906239167749b68960519ef6ebe8f4a63891
4thidiot <tendingtowardsinfinity@gmail.com>
2026-02-17 23:06:33 +0530

    Create 1372A - Omkar and Completion.cpp

---
+++ 1372A - Omkar and Completion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int c = 1; c <= t; c++) {
+        int n;
+        cin >> n;
+
+        for (int j = 1; j <= n; j++) {
+            cout << "1 ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
