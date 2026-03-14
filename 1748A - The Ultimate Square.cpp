db63bcf48a5800300b4a87aaffbabe9a5dfaaabd
4thidiot <tendingtowardsinfinity@gmail.com>
2026-03-14 22:54:34 +0530

    Create 1748A - The Ultimate Square.cpp

---
+++ 1748A - The Ultimate Square.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+void testcase(){
+    ll n;
+    cin>>n;
+    cout<<(n+1)/2<<'\n';
+}
+int main()
+{
+    ll t; cin>>t; while(t--)
+       testcase();
+    return 0;
+}
