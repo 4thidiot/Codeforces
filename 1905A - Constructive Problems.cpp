a7d93c1118c80f9d48818ddb7bef74a542de6158
4thidiot <tendingtowardsinfinity@gmail.com>
2026-03-28 22:26:26 +0530

    Create 1905A - Constructive Problems.cpp

---
+++ 1905A - Constructive Problems.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+//#define int ll
+#define sz(x) ((int)(x).size())
+
+using pii = pair<int,int>;
+using tii = tuple<int,int,int>;
+
+const int nmax = 1e6 + 5;
+const int inf = 1e9 + 5;
+int n, k, m, q;
+vector<int> g[nmax];
+int v[nmax];
+
+static void testcase() {
+  cin >> n >> m;
+  cout << max(n, m) << '\n';
+  return;
+}
+
+signed main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+    testcase();
+}
