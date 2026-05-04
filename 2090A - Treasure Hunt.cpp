1512fad556bcaab8a2110d79ea6381de53f5b37b
4thidiot <tendingtowardsinfinity@gmail.com>
2026-05-04 23:02:11 +0530

    Create 2090A - Treasure Hunt.cpp

---
+++ 2090A - Treasure Hunt.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int x, y, a;
+    cin >> x >> y >> a;
+    if (a % (x + y) < x) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
