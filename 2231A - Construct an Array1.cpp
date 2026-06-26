2f2e33f9c5daf45e7bd518b3585808ba497e1e6f
4thidiot <tendingtowardsinfinity@gmail.com>
2026-06-26 22:56:48 +0530

    Create 2231A - Construct an Array1.cpp

---
+++ 2231A - Construct an Array1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        for (int i = 1; i <= 2 * n - 1; i += 2)
+            cout << i << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
