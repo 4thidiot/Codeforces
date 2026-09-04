507bf0a7cbeaf6b7b6268aee7b66bdc0d8deb4c9
4thidiot <tendingtowardsinfinity@gmail.com>
2026-09-04 23:11:24 +0530

    Create 2182A - New Year String.cpp

---
+++ 2182A - New Year String.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        string s;
+
+        cin >> n >> s;
+
+        if(s.find("2025") != string::npos && s.find("2026") == string::npos)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}
