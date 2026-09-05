88c789535bde8ff774d5192515d311f9cd91b45b
4thidiot <tendingtowardsinfinity@gmail.com>
2026-09-05 23:31:53 +0530

    Create 2259A - Moo Language School.cpp

---
+++ 2259A - Moo Language School.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i += k)
+        {
+            bool possible = false;
+
+            for(int j = i; j < i + k; j++)
+            {
+                if(s[j] == '0')
+                {
+                    possible = true;
+                    break;
+                }
+            }
+
+            if(!possible)
+                ans++;
+        }
+
+        cout << ans << endl;
+    }
+}
