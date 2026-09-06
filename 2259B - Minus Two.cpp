3f217a5db3632f3b8d966ee4d9c1e73343f08915
4thidiot <tendingtowardsinfinity@gmail.com>
2026-09-06 22:57:42 +0530

    Add solution for problem 2259B - Minus Two

---
+++ 2259B - Minus Two.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        set<int> s;
+
+        for(int i = 0; i < n; i++)
+        {
+            while(a[i] >= 2)
+            {
+                a[i] -= 2;
+            }
+
+            s.insert(a[i]);
+        }
+
+        if(s.size() == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
