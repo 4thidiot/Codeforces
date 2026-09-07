d844fe75ec0d7d313bdef88588e00f79f7d0aa33
4thidiot <tendingtowardsinfinity@gmail.com>
2026-09-07 23:24:06 +0530

    Update 2120A - Square of Rectangles.cpp

--- 2120A - Square of Rectangles.cpp
+++ 2120A - Square of Rectangles.cpp
@@ -1,31 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    int t;
-    cin >> t;
+#define ll long long int
 
-    while (t--) {
-        int l1, b1, l2, b2, l3, b3;
-        cin >> l1 >> b1 >> l2 >> b2 >> l3 >> b3;
+int main()
+{
+    ll t;
+    cin>>t;
 
-        bool possible = false;
+    while(t--)
+    {
+        ll l1, b1, l2, b2, l3, b3;
+        cin>>l1>>b1>>l2>>b2>>l3>>b3;
 
-        // Case 1: All three rectangles placed side by side
-        if (b1 == b2 && b2 == b3 &&
-            l1 + l2 + l3 == b1) {
-            possible = true;
-        }
-
-        // Case 2: Largest rectangle on top, other two below
-        if (l1 == l2 + l3 &&
-            b2 == b3 &&
-            b1 + b2 == l1) {
-            possible = true;
-        }
-
-        cout << (possible ? "YES" : "NO") << '\n';
+        if(l1+l2+l3 == b1 && b1==b2 && b2==b3) cout<<"YES\n";
+        else if(l2+l3 == l1 && b2==b3 && b1+b2==l1) cout<<"YES\n";
+        else if(b1+b2+b3 == l1 && l1==l2 && l2==l3) cout<<"YES\n";
+        else if(b2+b3 == b1 && l2==l3 && l1+l2==b1) cout<<"YES\n";
+        else cout<<"NO\n";
     }
-
-    return 0;
 }
