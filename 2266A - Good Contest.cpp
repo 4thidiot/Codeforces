536af5b90a7f2cce88851800cb7ebb28ed2d9fb5
4thidiot <tendingtowardsinfinity@gmail.com>
2026-09-22 23:17:09 +0530

    Create 2266A - Good Contest.cpp

---
+++ 2266A - Good Contest.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, a, b, c;
+        cin >> n;
+        cin >> a >> b >> c;
+
+        cout << n - min({a, b, c}) << '\n';
+    }
+
+    return 0;
+}
